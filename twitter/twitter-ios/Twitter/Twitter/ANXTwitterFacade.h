a993b323174492c9456bb919596371dcd82bd264
Max Rozdobudko <max.rozdobudko@gmail.com>
2014-11-21 18:55:15 +0200

    Twitter iOS part

--- twitter/twitter-ios/Twitter/Twitter/ANXTwitterFacade.h
+++ twitter/twitter-ios/Twitter/Twitter/ANXTwitterFacade.h
@@ -8,6 +8,38 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Twitter : NSObject
+#import <Twitter/Twitter.h>
+
+#import "FlashRuntimeExtensions.h"
+
+#import "ANXFabricConversionRoutines.h"
+
+@interface ANXTwitterFacade : NSObject
 
 @end
+
+#pragma mark API
+
+FREObject ANXTwitterIsSupported(FREContext context, void* functionData, uint32_t argc, FREObject argv[]);
+
+
+FREObject ANXTwitterLogin(FREContext context, void* functionData, uint32_t argc, FREObject argv[]);
+
+FREObject ANXTwitterLogout(FREContext context, void* functionData, uint32_t argc, FREObject argv[]);
+
+
+FREObject ANXTwitterLoginGuest(FREContext context, void* functionData, uint32_t argc, FREObject argv[]);
+
+FREObject ANXTwitterLogoutGuest(FREContext context, void* functionData, uint32_t argc, FREObject argv[]);
+
+#pragma mark ContextInitialize/ContextFinalizer
+
+void ANXTwitterContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, uint32_t* numFunctionsToTest, const FRENamedFunction** functionsToSet);
+
+void ANXTwitterContextFinalizer(FREContext ctx);
+
+#pragma mark Initializer/Finalizer
+
+void ANXTwitterInitializer(void** extDataToSet, FREContextInitializer* ctxInitializerToSet, FREContextFinalizer* ctxFinalizerToSet);
+
+void ANTwitterinalizer(void* extData);
--- twitter/twitter-ios/Twitter/Twitter/ANXTwitterFacade.m
+++ twitter/twitter-ios/Twitter/Twitter/ANXTwitterFacade.m
@@ -6,8 +6,91 @@
 //  Copyright (c) 2014 Max Rozdobudko. All rights reserved.
 //
 
-#import "Twitter.h"
+#import "ANXTwitterFacade.h"
 
-@implementation Twitter
+@implementation ANXTwitterFacade
 
 @end
+
+FREObject ANXTwitterIsSupported(FREContext context, void* functionData, uint32_t argc, FREObject argv[])
+{
+    return [ANXFabricConversionRoutines convertBoolToFREObject: YES];
+}
+
+FREObject ANXTwitterLogin(FREContext context, void* functionData, uint32_t argc, FREObject argv[])
+{
+    [Twitter sharedInstance];
+    
+    return NULL;
+}
+
+FREObject ANXTwitterLogout(FREContext context, void* functionData, uint32_t argc, FREObject argv[])
+{
+    return NULL;
+}
+
+
+FREObject ANXTwitterLoginGuest(FREContext context, void* functionData, uint32_t argc, FREObject argv[])
+{
+    return NULL;
+}
+
+FREObject ANXTwitterLogoutGuest(FREContext context, void* functionData, uint32_t argc, FREObject argv[])
+{
+    return NULL;
+}
+
+#pragma mark ContextInitialize/ContextFinalizer
+
+void ANXTwitterContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, uint32_t* numFunctionsToTest, const FRENamedFunction** functionsToSet)
+{
+    NSLog(@"ANXTwitterContextInitializer");
+    
+    *numFunctionsToTest = 5;
+    
+    FRENamedFunction* func = (FRENamedFunction*) malloc(sizeof(FRENamedFunction) * (*numFunctionsToTest));
+    
+    func[0].name = (const uint8_t*) "isSupported";
+    func[0].functionData = NULL;
+    func[0].function = &ANXTwitterIsSupported;
+    
+    func[1].name = (const uint8_t*) "login";
+    func[1].functionData = NULL;
+    func[1].function = &ANXTwitterLogin;
+    
+    func[2].name = (const uint8_t*) "logout";
+    func[2].functionData = NULL;
+    func[2].function = &ANXTwitterLogout;
+    
+    func[3].name = (const uint8_t*) "loginGuest";
+    func[3].functionData = NULL;
+    func[3].function = &ANXTwitterLoginGuest;
+    
+    func[4].name = (const uint8_t*) "logoutGuest";
+    func[4].functionData = NULL;
+    func[4].function = &ANXTwitterLogoutGuest;
+
+    *functionsToSet = func;
+}
+
+void ANXTwitterContextFinalizer(FREContext ctx)
+{
+    NSLog(@"ANXTwitterContextFinalizer");
+}
+
+#pragma mark Initializer/Finalizer
+
+void ANXTwitterInitializer(void** extDataToSet, FREContextInitializer* ctxInitializerToSet, FREContextFinalizer* ctxFinalizerToSet)
+{
+    NSLog(@"ANXTwitterInitializer");
+    
+    *extDataToSet = NULL;
+    
+    *ctxInitializerToSet = &ANXTwitterContextInitializer;
+    *ctxFinalizerToSet = &ANXTwitterContextFinalizer;
+}
+
+void ANTwitterFinalizer(void* extData)
+{
+    NSLog(@"ANTwitterFinalizer");
+}
